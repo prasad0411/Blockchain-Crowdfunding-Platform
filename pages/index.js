f0a1d35644fbd201ae2821acadb92bc036c52d57
prasad0411 <prasadckanade@gmail.com>
2023-04-30 12:50:51 +0530

    modified homepage

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,6 @@ class HomePage extends Component {
                     <title>Home Page</title>
                     <link rel="stylesheet" type="text/css" href="/homepage.css" />
                 </Head>
-
                 <div className="connect-container">
                     <button
                         className={`btn btn-sm btn-primary rounded-pill mr-2
@@ -40,7 +39,7 @@ class HomePage extends Component {
 
                         }}
                     >
-                        Login
+                        Login<span className="arrow">&#8594;</span>
                     </button >
                 </div>
                 <div className="container">
@@ -52,33 +51,34 @@ class HomePage extends Component {
                             <div className="card-title">What have we achieved?</div>
                             <div className="card-text">
                                 Through the use of an internet platform, crowdfunding has grown to
-                                {/* be a well-liked method for creative people to raise money for their
-                            projects. However, there are a number of problems with the present
-                            crowdfunding model, including the use of third-party middlemen who
-                            cannot guarantee the investor's money and a lack of investor control
-                            over the use of their money. The secure, dependable, and
-                            decentralised network of blockchain technology, on the other hand,
-                            has grown to be quite popular across a wide range of businesses due
-                            to its efficiency when compared to more conventional approaches.
-                            Yet, because of their complicated and less secure networks, older
-                            approaches have a lot of problems and difficulties. These issues can
-                            be solved by incorporating blockchain technology, which offers
-                            advantages like improved security, transparency, efficiency, and
-                            reduced fraud risk. We suggest a new crowdfunding platform built on
-                            blockchain technology that offers a decentralised and safe method of
-                            crowdfunding to address these problems. Our platform's main goal is
-                            to give investors the ability to successfully contribute to any
-                            project by enabling them to create smart contracts that give them
-                            control over the invested funds. As a result, project developers are
-                            prevented from using funding in a manner with which investors
-                            disagreed. Our work emphasises the advantages of integrating
-                            blockchain in different industries while highlighting the contrasts
-                            between conventional and blockchain-based crowdfunding platforms. We
-                            also go through the difficulties traditional industries face and how
-                            blockchain-based platforms might help solve these issues. Our goal
-                            is to raise awareness of the advantages of blockchain technology in
-                            various industries and to promote its adoption to increase security,
-                            efficiency, and transparency. */}
+                                be a well-liked method for creative people to raise money for their
+                                projects. However, there are a number of problems with the present
+                                crowdfunding model, including the use of third-party middlemen who
+                                cannot guarantee the investor's money and a lack of investor control
+                                over the use of their money.
+                                <p></p>The secure, dependable, and
+                                decentralised network of blockchain technology, on the other hand,
+                                has grown to be quite popular across a wide range of businesses due
+                                to its efficiency when compared to more conventional approaches.
+                                Yet, because of their complicated and less secure networks, older
+                                approaches have a lot of problems and difficulties. These issues can
+                                be solved by incorporating blockchain technology, which offers
+                                advantages like improved security, transparency, efficiency, and
+                                reduced fraud risk. <p></p>We suggest a new crowdfunding platform built on
+                                blockchain technology that offers a decentralised and safe method of
+                                crowdfunding to address these problems. Our platform's main goal is
+                                to give investors the ability to successfully contribute to any
+                                project by enabling them to create smart contracts that give them
+                                control over the invested funds. As a result, project developers are
+                                prevented from using funding in a manner with which investors
+                                disagreed.<p></p> Our work emphasises the advantages of integrating
+                                blockchain in different industries while highlighting the contrasts
+                                between conventional and blockchain-based crowdfunding platforms. We
+                                also go through the difficulties traditional industries face and how
+                                blockchain-based platforms might help solve these issues. Our goal
+                                is to raise awareness of the advantages of blockchain technology in
+                                various industries and to promote its adoption to increase security,
+                                efficiency, and transparency.
                             </div>
                         </div>
                     </div>
@@ -104,7 +104,8 @@ class HomePage extends Component {
                         </div>
                     </div>
                     <div className="button-container">
-                        <button className="next-button" onClick={() => { Router.pushRoute('/allProjects') }}>
+                        <button className="next-button"
+                            onClick={() => { Router.pushRoute('/allProjects') }}>
                             View all projects<span className="arrow">&#8594;</span>
                         </button>
                     </div>
